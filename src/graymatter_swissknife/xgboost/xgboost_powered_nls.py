97bf22ad299f51660b872f507100779386684e16
Quentin Uhl <quentin.uhl@wanadoo.fr>
2025-04-11 19:29:53 +0200

    Update xgboost_powered_nls.py

--- src/graymatter_swissknife/xgboost/xgboost_powered_nls.py
+++ src/graymatter_swissknife/xgboost/xgboost_powered_nls.py
@@ -23,15 +23,19 @@ def xgboost_powered_nls_loop(xgboost_model, target_signal, microstruct_model, nl
     """
     # Initial solution
     assert initial_gt is not None, "Initial ground truth must be provided for XGBoost-powered NLS."
-    x_0 = initial_gt
 
     # Function to optimize using the XGBoost model
     if not microstruct_model.has_noise_correction:
+        x_0 = initial_gt
         optim_fun = lambda x: np.sum(np.square(xgboost_model.predict(np.array([x]))[0] - target_signal))
+        # Optimisation function
+        result = op.minimize(fun=optim_fun, x0=x_0, method='L-BFGS-B', bounds=nls_param_lim, tol=1e-14)
     else:
-        optim_fun = lambda x: np.sum(np.square(rice_mean(xgboost_model.predict(np.array([x[:-1]]))[0], x[-1]) - target_signal))
-    # Optimisation function
-    result = op.minimize(fun=optim_fun, x0=x_0, method='L-BFGS-B', bounds=nls_param_lim, tol=1e-14)
+        x_0 = initial_gt[:-1]
+        sigma = initial_gt[-1]
+        optim_fun = lambda x: np.sum(np.square(rice_mean(xgboost_model.predict(np.array([x]))[0], sigma) - target_signal))
+        # Optimisation function
+        result = op.minimize(fun=optim_fun, x0=x_0, method='L-BFGS-B', bounds=nls_param_lim, tol=1e-14)
     x_sol = result.x
     return [x_sol, x_0]
 
@@ -51,14 +55,19 @@ def xgboost_powered_nls_parallel(xgboost_model, signal, N, microstruct_model, nl
     """
     x_0 = np.empty([N, microstruct_model.n_params])
     x_sol = np.empty([N, microstruct_model.n_params])
-    if initial_gt is None:
-        initial_gt = [None for _ in range(N)]
     logging.info('Running XGBoost powered Non-linear Least Squares')
+
+    # Change the limits to [0, 1] for the XGBoost model
+    if not microstruct_model.has_noise_correction:
+        xgb_nls_param_lim = np.array([[0, 1] for _ in range(microstruct_model.n_params)])
+    else:
+        sigma = initial_gt[:, -1]
+        xgb_nls_param_lim = np.array([[0, 1] for _ in range(microstruct_model.n_params - 1)])
 
     x_parallel = []
     for irunning in tqdm(range(N)):
         x_parallel.append(xgboost_powered_nls_loop_verified(
-            xgboost_model, signal[irunning], microstruct_model, nls_param_lim, initial_gt[irunning], max_nls_verif
+            xgboost_model, signal[irunning], microstruct_model, xgb_nls_param_lim, initial_gt[irunning], max_nls_verif
         ))
 
     # NLS_loop_verified is NLS_loop run several times until NLS doesn't output any boundary. Defined below.
@@ -67,6 +76,33 @@ def xgboost_powered_nls_parallel(xgboost_model, signal, N, microstruct_model, nl
         x_sol[index] = x_parallel[index][0]
         x_0[index] = x_parallel[index][1]
         verif_failed_count += x_parallel[index][2]
+
+    # Multiply the results by the limits to get the real values (XGBoost version)
+    # x_sol and x_0 are in [0, 1] so we need to multiply by the limits to get the real values
+    if not microstruct_model.has_noise_correction:
+        for param_index in range(microstruct_model.n_params):
+            x_sol[:, param_index] = (
+                x_sol[:, param_index] * (nls_param_lim[param_index][1] - nls_param_lim[param_index][0])
+                + nls_param_lim[param_index][0]
+            )
+            x_0[:, param_index] = (
+                x_0[:, param_index] * (nls_param_lim[param_index][1] - nls_param_lim[param_index][0])
+                + nls_param_lim[param_index][0]
+            )
+    else:
+        for param_index in range(microstruct_model.n_params - 1):
+            x_sol[:, param_index] = (
+                x_sol[:, param_index] * (nls_param_lim[param_index][1] - nls_param_lim[param_index][0])
+                + nls_param_lim[param_index][0]
+            )
+            x_0[:, param_index] = (
+                x_0[:, param_index] * (nls_param_lim[param_index][1] - nls_param_lim[param_index][0])
+                + nls_param_lim[param_index][0]
+            )
+        # sigma is the last parameter
+        x_sol[:, -1] = sigma
+        x_0[:, -1] = sigma
+
     logging.info(f"Failed {max_nls_verif}-times border verification procedure : {verif_failed_count} out of {N}")
     logging.info('Non-linear Least Squares completed')
     return x_sol, x_0
