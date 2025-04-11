650c6a2ba548b90f038a9c5fdc571ef4bd02cd9e
Quentin Uhl <quentin.uhl@wanadoo.fr>
2025-04-11 14:04:48 +0200

    Update XGBoost powered NLS V1

---
+++ src/graymatter_swissknife/xgboost/xgboost_powered_gridsearch.py
@@ -0,0 +1,157 @@
+import logging
+import numpy as np
+from tqdm import tqdm
+import itertools
+from joblib import Parallel, delayed
+from ..models.noise.rice_mean import rice_mean
+
+
+def find_nls_initialization_with_xgboost(
+    xgboost_model, signal, sigma, nb_estimates, acq_param, microstruct_model, grid_search_param_lim, grid_search_nb_points, debug=False
+):
+    """
+    Find the initialization of the Non-linear least squares algorithm.
+    :param signal: signal for all b-values (shells) and diffusion times (Deltas)
+    :param sigma: noise standard deviation
+    :param nb_estimates: number of ground truth to estimate
+    :param acq_param: acquisition parameters
+    :param microstruct_model: microstructure model
+    :param grid_search_param_lim: bounds for the grid search
+    :param debug: if True, print debug information
+    :return: initial_gt: array of optimized initial ground truths for the NLS algorithm
+    """
+    # Access to grid search hyperparameters
+    n_cores = -1  # multiprocessing.cpu_count()
+    overlap = 1  # from 0 to 1 : 0 mean no overlap of the grid ranges, 1 means complete overlap
+
+    # Check that the number of parameters in the grid search and in the model are the same
+    assert microstruct_model.n_params == len(
+        grid_search_param_lim
+    ), "Number of parameters in the grid search and in the model are different"
+
+    # Define number of moving parameters
+    if microstruct_model.has_noise_correction:
+        n_moving_param = microstruct_model.n_params - 1  # -1 because sigma is fixed
+    else:
+        n_moving_param = microstruct_model.n_params
+
+    logging.info(
+        f"Initializing first grid search inside :\n{grid_search_param_lim}\nusing model {microstruct_model.name}\n..."
+    )
+
+    # Generate the parameter grid combinations (avoiding the limits of the bounds)
+    param_grid = []
+    for p in range(n_moving_param):
+        if grid_search_param_lim[p][0] == grid_search_param_lim[p][1]:
+            param_grid.append([grid_search_param_lim[p][0]])
+        else:
+            param_grid.append(
+                np.linspace(grid_search_param_lim[p][0], grid_search_param_lim[p][1], grid_search_nb_points[p] + 2)[1:-1].tolist()
+            )
+    grid_combinations = list(itertools.product(*param_grid))
+    parameters = np.array(grid_combinations)
+
+    # Generate the non-corrected signal dictionary (or grid)
+    if microstruct_model.has_noise_correction:
+        non_corrected_model = microstruct_model.non_corrected_model
+        logging.info(f"Generating {non_corrected_model.name} signal dictionary using the XGBoost trained model")
+    else:
+        logging.info(f"Generating {microstruct_model.name} signal dictionary using the XGBoost trained model")
+    
+    signal_dict = np.array(
+        Parallel(n_jobs=n_cores)(
+            delayed(xgboost_model.predict)(params) for params in tqdm(grid_combinations)
+        )
+    )
+
+    if debug:
+        logging.info(f"Signal dictionary shape : {signal_dict.shape}")
+
+    # Find the initial ground truth whether the model has a Rician mean correction or not
+    logging.info("Extracting initial Ground Truth from dictionary")
+    if microstruct_model.has_noise_correction:
+        # Noise correction case : Rice noise
+        initial_gt = np.array(
+            Parallel(n_jobs=n_cores)(
+                delayed(least_square_argmin_rician_corrected)(
+                    signal[i], sigma[i], signal_dict, parameters, acq_param.ndim
+                )
+                for i in tqdm(range(nb_estimates))
+            )
+        )
+        initial_gt = np.hstack((initial_gt, np.array([sigma]).T))
+    else:
+        initial_gt = np.array(
+            Parallel(n_jobs=n_cores)(
+                delayed(least_square_argmin)(signal[i], signal_dict, parameters, acq_param.ndim)
+                for i in tqdm(range(nb_estimates))
+            )
+        )
+    if debug:
+        logging.info(f'initial_gt shape : {initial_gt.shape}')
+
+    # Add randomness to the initial GT
+    if microstruct_model.has_noise_correction:
+        # If the model has noise correction, we need to add randomness to the initial GT except to sigma (-1)
+        for parameter in range(len(grid_search_param_lim) - 1):
+            initial_gt[:, parameter] += (
+                (1 + overlap)
+                * (grid_search_param_lim[parameter][1] - grid_search_param_lim[parameter][0])
+                / (grid_search_nb_points[parameter] + 1)
+                * (np.random.rand(nb_estimates) - 0.5)
+            )
+    else:
+        for parameter in range(len(grid_search_param_lim)):
+            initial_gt[:, parameter] += (
+                (1 + overlap)
+                * (grid_search_param_lim[parameter][1] - grid_search_param_lim[parameter][0])
+                / (grid_search_nb_points[parameter] + 1)
+                * (np.random.rand(nb_estimates) - 0.5)
+            )
+
+    # Print the first initial ground truth to check
+    if debug:
+        for p_ind, param_name in enumerate(microstruct_model.param_names):
+            logging.info(f'First initial_gt {param_name} : {initial_gt[0, p_ind]}')
+    logging.info("First Grid Search completed\n")
+    return initial_gt
+
+
+def least_square_argmin(signal_i, signal_dict, parameters, acq_param_ndim):
+    """
+    Compute the argmin of the least squares function for a given signal.
+    :param signal_i: signal for all b-values (shells) and diffusion times (Deltas)
+    :param signal_dict: signal dictionary
+    :param parameters: parameters of the signal dictionary
+    :param acq_param_ndim: number of dimensions of the acquisition parameters
+    :return: argmin of the least squares function
+    """
+    if acq_param_ndim == 2:
+        lstsqr = np.sum(np.square(np.expand_dims(signal_i, axis=0) - signal_dict), axis=(-2, -1))
+    elif acq_param_ndim == 1:
+        lstsqr = np.sum(np.square(np.expand_dims(signal_i, axis=0) - signal_dict), axis=-1)
+    else:
+        raise NotImplementedError
+    return parameters[np.unravel_index(lstsqr.argmin(), lstsqr.shape)]
+
+
+def least_square_argmin_rician_corrected(signal_i, sigma_i, initial_signal_dict, parameters, acq_param_ndim):
+    """
+    Compute the argmin of the least squares function for a given not yet corrected signal and noise level.
+    :param signal_i: signal for all b-values (shells) and diffusion times (Deltas)
+    :param signal_dict: signal dictionary
+    :param parameters: parameters of the signal dictionary
+    :param acq_param_ndim: number of dimensions of the acquisition parameters
+    :return: argmin of the least squares function
+    """
+    # Update the signal dictionary with the noise level
+    corrected_signal_dict = rice_mean(initial_signal_dict, sigma_i)
+    # Compute the argmin of the least squares function
+    if acq_param_ndim == 2:
+        lstsqr = np.sum(np.square(np.expand_dims(signal_i, axis=0) - corrected_signal_dict), axis=(-2, -1))
+    elif acq_param_ndim == 1:
+        lstsqr = np.sum(np.square(np.expand_dims(signal_i, axis=0) - corrected_signal_dict), axis=-1)
+    else:
+        raise NotImplementedError
+    return parameters[np.unravel_index(lstsqr.argmin(), lstsqr.shape)]
+
---
+++ src/graymatter_swissknife/xgboost/xgboost_powered_nls.py
@@ -0,0 +1,126 @@
+# Implementation of Non-linear Least Squares
+import logging
+import numpy as np
+import scipy.optimize as op
+from tqdm import tqdm
+from joblib import Parallel, delayed
+from ..models.noise.rice_mean import rice_mean
+
+####################################################################
+# Parallelized NLS
+####################################################################
+
+
+def xgboost_powered_nls_loop(xgboost_model, target_signal, microstruct_model, acq_param, nls_param_lim, initial_gt=None):
+    """
+    Non-linear least squares algorithm for a single ground truth.
+    :param target_signal: signal for all b-values (shells) and diffusion times (Deltas)
+    :param microstruct_model: microstructure model
+    :param acq_param: acquisition parameters
+    :param nls_param_lim: bounds for the estimation in the non-linear least squares algorithm
+    :param initial_gt: the initial ground truth to start the optimization from. If None, random initializations are used.
+    :return: x_sol: estimated ground truth
+    :return: x_0: initial ground truth
+    """
+    # Initial solution
+    assert initial_gt is not None, "Initial ground truth must be provided for XGBoost-powered NLS."
+    x_0 = initial_gt
+
+    # Function to optimize using the XGBoost model
+    if not microstruct_model.has_noise_correction:
+        optim_fun = lambda x: np.sum(np.square(xgboost_model.predict(x) - target_signal))
+    else:
+        optim_fun = lambda x: np.sum(np.square(rice_mean(xgboost_model.predict(x[:-1]), x[-1]) - target_signal))
+    # Optimisation function
+    result = op.minimize(fun=optim_fun, x0=x_0, method='L-BFGS-B', bounds=nls_param_lim, tol=1e-14)
+    x_sol = result.x
+    return [x_sol, x_0]
+
+
+def xgboost_powered_nls_parallel(xgboost_model, signal, N, microstruct_model, acq_param, nls_param_lim, max_nls_verif=5, initial_gt=None, n_cores=-1):
+    """
+    Parallelized version of the non-linear least squares algorithm.
+
+    :param signal: signal for all b-values (shells) and diffusion times (Deltas)
+    :param N: number of ground truth to estimate
+    :param microstruct_model: microstructure model to use
+    :param acq_param: acquisition parameters
+    :param nls_param_lim: bounds for the estimation in the non-linear least squares algorithm
+    :param max_nls_verif: maximum number of times the non-linear least squares algorithm is run for each ground truth
+    :param initial_gt: the initial ground truth to start the optimization from. If None, random initializations are used.
+    :param n_cores: number of cores to use for the parallelization. If -1, all available cores are used.
+    :return: x_sol: estimated ground truth
+    :return: x_0: initial ground truth
+    """
+    x_0 = np.empty([N, microstruct_model.n_params])
+    x_sol = np.empty([N, microstruct_model.n_params])
+    if initial_gt is None:
+        initial_gt = [None for _ in range(N)]
+    logging.info('Running Parallelized Non-linear Least Squares')
+    if hasattr(microstruct_model, 'get_mse_jacobian'):
+        logging.info("Optimization using implemented jacobian.")
+    else:
+        logging.info("Warning : Jacobian not implemented for this model. Optimization will be slower.")
+    x_parallel = Parallel(n_jobs=n_cores)(
+        delayed(xgboost_powered_nls_loop_verified)(
+            xgboost_model, signal[irunning], microstruct_model, acq_param, nls_param_lim, initial_gt[irunning], max_nls_verif
+        )
+        for irunning in tqdm(range(N))
+    )  # n_jobs=-1, verbose=50 for verbose
+    # NLS_loop_verified is NLS_loop run several times until NLS doesn't output any boundary. Defined below.
+    verif_failed_count = 0
+    for index in range(N):
+        x_sol[index] = x_parallel[index][0]
+        x_0[index] = x_parallel[index][1]
+        verif_failed_count += x_parallel[index][2]
+    logging.info(f"Failed {max_nls_verif}-times border verification procedure : {verif_failed_count} out of {N}")
+    logging.info('Non-linear Least Squares completed')
+    return x_sol, x_0
+
+
+####################################################################
+# Verification of each NLS loop max_nls_verif times
+####################################################################
+
+
+def touch_border(x_sol, nls_param_lim, n_param):
+    """
+    Check if the NLS algorithm has touched the border of the parameter space.
+    :param x_sol: estimated ground truth
+    :param nls_param_lim: bounds for the estimation in the non-linear least squares algorithm
+    :param n_param: number of parameters
+    :return: True if the NLS algorithm has touched the border of the parameter space, False otherwise
+    """
+    for ind_p in range(n_param):
+        if nls_param_lim[ind_p][0] != nls_param_lim[ind_p][1]:
+            if x_sol[ind_p] == nls_param_lim[ind_p][0] or x_sol[ind_p] == nls_param_lim[ind_p][1]:
+                return True
+    return False
+
+
+def xgboost_powered_nls_loop_verified(xgboost_model, target_signal, microstruct_model, acq_param, nls_param_lim, initial_gt=None, max_nls_verif=5):
+    """
+    Verification of each NLS loop max_nls_verif times.
+    :param target_signal: signal for all b-values (shells) and diffusion times (Deltas)
+    :param microstruct_model: microstructure model
+    :param acq_param: acquisition parameters
+    :param nls_param_lim: bounds for the estimation in the non-linear least squares algorithm
+    :param initial_gt: the initial ground truth to start the optimization from. If None, random initializations are used.
+    :param max_nls_verif: maximum number of times the non-linear least squares algorithm is run for each ground truth
+    :return: x_sol: estimated ground truth
+    :return: x_0: initial ground truth
+    :return: verif_failed: array of booleans indicating if the NLS algorithm has touched the border of the parameter space
+    """
+    nls_loop_return = xgboost_powered_nls_loop(xgboost_model, target_signal, microstruct_model, acq_param, nls_param_lim, initial_gt)
+    x_sol, x_0 = nls_loop_return[0], nls_loop_return[1]
+    iter_nb = 1
+    while touch_border(x_sol, nls_param_lim, microstruct_model.n_params) and iter_nb < max_nls_verif:
+        nls_loop_return = xgboost_powered_nls_loop(xgboost_model, target_signal, microstruct_model, acq_param, nls_param_lim, initial_gt=None)
+        x_sol, x_0 = nls_loop_return[0], nls_loop_return[1]
+        iter_nb += 1
+    if touch_border(x_sol, nls_param_lim, microstruct_model.n_params) and iter_nb == max_nls_verif:
+        verif_failed = 1
+        # logging.info("Border touched " + str(int(max_nls_verif)) + " times !")
+    else:
+        verif_failed = 0
+    return [x_sol, x_0, verif_failed]
